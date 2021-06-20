e5bf710de96d0e6a18a5528d1dec8a9501874d7b
Anique Tahir (Student) <artahir@en4202093l.fulton.ad.asu.edu>
2021-06-19 19:55:56 -0700

    Added visualization page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import PostDisplay from "../components/post-display";
 import useSWR from "swr";
+import DeckGL from '@deck.gl/react';
+import {COORDINATE_SYSTEM, OrbitView, LinearInterpolator} from '@deck.gl/core';
+import {LineLayer, PointCloudLayer} from '@deck.gl/layers';
+import {LASWorkerLoader} from '@loaders.gl/las'
+import {useState, useEffect} from "react";
+
 
 export const fetcher = (...args)=>fetch(...args).then(res=>res.json());
 
@@ -16,13 +22,132 @@ export async function getStaticProps() {
     }
 }
 
+function DeckTestPCL({onLoad}){
+    const LAZ_SAMPLE = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/point-cloud-laz/indoor.0.1.laz';
+    const INITIAL_VIEW_STATE = {
+        target: [0,0,0],
+        rotationX: 0,
+        rotationOrbit: 0,
+        orbitAxis: 'Y',
+        fov: 50,
+        minZoom: 0,
+        maxZoom: 10,
+        zoom: 1
+    }
+
+    const transitionInterpolator = new LinearInterpolator(['rotationOrbit']);
+
+    const [viewState, updateViewState] = useState(INITIAL_VIEW_STATE);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [hoverInfo, setHoverInfo] = useState();
+
+    useEffect(
+        () => {
+            if(!isLoaded){
+                return;
+            }
+
+            const rotateCamera = () => {
+                updateViewState(v => ({
+                  ...v,
+                  rotationOrbit: v.rotationOrbit + 120,
+                  transitionDuration: 2400,
+                  transitionInterpolator,
+                  onTransitionEnd: rotateCamera
+                }));
+            };
+            rotateCamera();
+        },
+        [isLoaded]
+    );
+
+    const onDataLoad = ({header}) => {
+        if (header.boundingBox) {
+            const [mins, maxs] = header.boundingBox;
+            updateViewState({
+                ...INITIAL_VIEW_STATE,
+                target: [(mins[0] + maxs[0])/2, (mins[1] + maxs[1])/2, (mins[2] + maxs[2])/2],
+                zoom: Math.log2(window.innerWidth / (maxs[0] - mins[0])) - 1
+            });
+            setIsLoaded(true);
+        }
+
+        if (onLoad) {
+            onLoad({count: header.vertexCount, progress: 1});
+        }
+    };
+
+    const layers = [
+        new PointCloudLayer({
+            id: 'laz-point-cloud-layer',
+            data: LAZ_SAMPLE,
+            onDataLoad,
+            coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
+            getNormal: [0, 1, 0],
+            getColor: [255, 255, 255],
+            opacity: 0.5,
+            pointSize: 0.5,
+            pickable: true,
+            loaders: [LASWorkerLoader],
+            onHover: info => setHoverInfo(info),
+            onClick: info => console.log(info)
+        })
+    ];
+
+    return (
+        <DeckGL views={new OrbitView()}
+                viewState={viewState}
+                controller={true}
+                onViewStateChange={v => updateViewState(v.viewState)}
+                layers={layers}
+                parameters = {{
+                    clearColor: [0.93, 0.86, 0.81, 1]
+                }}
+        >
+            {/*{hoverInfo.object && (*/}
+            {/*    <div style={{position: 'absolute', zIndex: 1, pointerEvents: 'none', left: hoverInfo.x, top: hoverInfo.y}}>*/}
+            {/*        hello*/}
+            {/*    </div>*/}
+            {/*)}*/}
+
+        </DeckGL>
+    );
+}
+
+export function DeckLayer(){
+    const INITIAL_VIEW_STATE = {
+        longitude: -122.41669,
+        latitude: 37.7853,
+        zoom: 13,
+        pitch: 0,
+        bearing: 0
+    };
+
+    // Data for Line layer
+    const data = [
+        {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+    ];
+
+    const layers = [
+        new LineLayer({id: 'line-layer', data})
+    ];
+
+    return (
+        <DeckGL initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={layers}
+        />
+    );
+}
+
 export default function Home({allPostsData}) {
   return (
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
         </Head>
-          <PostDisplay posts={useSWR('api/posts_sample', fetcher, {refreshInterval: 10000})} />
+          <DeckTestPCL onLoad={true} />
+          {/* <PostDisplay posts={useSWR('api/posts_sample', fetcher, {refreshInterval: 10000})} /> */}
         {/*<section className={utilStyles.headingMd}>*/}
         {/*  <p>[Your Self Introduction]</p>*/}
         {/*  <p>*/}
